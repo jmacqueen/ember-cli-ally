2ea14615783803ece2c0ec51d38b604ae3cbd279
Jonathan MacQueen <jmacqueen@primerevenue.com>
2016-01-19 13:40:22 -0500

    update index.js and bump version

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = {
   name: 'ember-cli-ally',
 
   included: function(app) {
-    this._super.included(app);
-    this.app.import(app.bowerDirectory + '/ally/ally.min.js');
-    this.app.import('vendor/shims/ally.js');
+    this._super.included.apply(this, [app]);
+    app.import(app.bowerDirectory + '/ally/ally.min.js');
+    app.import('vendor/shims/ally.js');
   }
 };
